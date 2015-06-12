8becc8bd985c36cdb66765e9f17323b0d2222832
Scott Wales <scott.wales@unimelb.edu.au>
2015-06-12 21:03:51 +1000

    Try and fix imports for Travis

--- ftools/parser/__init__.py
+++ ftools/parser/__init__.py
@@ -2,4 +2,3 @@ from parser            import parse
 from Fortran03Lexer    import Fortran03Lexer
 from Fortran03Parser   import Fortran03Parser
 from Fortran03Listener import Fortran03Listener
-from Parser import Parser
